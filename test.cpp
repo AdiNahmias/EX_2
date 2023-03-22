5b5f5af890cf7ee7b9652b6fc346477af8f251fd
AdiNahmias <adi.nahmias15@gmail.com>
2023-03-22 11:39:45 +0200

    test 3

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 
-TEST_CASE("when we start the game"){
+TEST_CASE("Amount of cards per player at the start of the game"){
     Player p1("Alice");
     Player p2("Bob");
     //create new game
@@ -22,13 +22,32 @@ TEST_CASE("when we start the game"){
     CHECK(division);
 }
 
-TEST_CASE("when we finish the game"){
+TEST_CASE("Amount of cards per player at the end of the game"){
     Player p1("Alice");
     Player p2("Bob");
     //create new game
     Game game(p1, p2);
+    game.playAll();
     bool division;
     if((p1.stacksize == 52 && p2.stacksize == 0) || (p2.stacksize == 52 && p1.stacksize == 0)){
+        division = true;
+    }else{
+        division = false;
+    }
+    CHECK(division);
+
+}
+TEST_CASE("Amount of cards per player in the middle of the game"){
+     Player p1("Alice");
+    Player p2("Bob");
+    //create new game
+    Game game(p1, p2);
+    // After 5 game turns
+    for (int i=0; i<5 ;i++) {
+    game.playTurn();
+    }
+    bool division;
+    if(p1.stacksize + p2.stacksize == 52){
         division = true;
     }else{
         division = false;
