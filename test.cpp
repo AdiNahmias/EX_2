412afc4c6803821f686a50eb62b4c35204ffaf08
AdiNahmias <adi.nahmias15@gmail.com>
2023-03-22 11:09:45 +0200

    test 1

--- test.cpp
+++ test.cpp
@@ -2,9 +2,20 @@
 #include <stdexept>
 #include "iostream"
 #include "stdio.h"
+#include "player.hpp"
+#include "game.hpp"
+#include "card.hpp"
 using namespace std;
 
 
-TEST_CASE("FIRST CASE"){
-    CHECK()
+TEST_CASE("when we start the game"){
+    Player p1;
+    Player p2;
+    //create new game
+    Game game(p1, p2);
+    bool division;
+    if(p1.stacksize == 26 && p2.stacksize == 26){
+        division = true;
+    }
+    CHECK(division);
 }
