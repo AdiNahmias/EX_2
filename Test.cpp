8d9faeeba7979c11cbb8135267dfb7471c003eb6
AdiNahmias <adi.nahmias15@gmail.com>
2023-03-26 00:02:17 +0300

    finish

--- Test.cpp
+++ Test.cpp
@@ -8,6 +8,15 @@
 using namespace std;
 
 
+TEST_CASE("Checking the amount of cards for each player before creating a game"){
+    Player p1("Alice");
+    Player p2("Bob");
+    CHECK(p1.stacksize() == 0);
+    CHECK(p2.stacksize() == 0);
+    CHECK(p1.cardesTaken() == 0);
+    CHECK(p2.cardesTaken() == 0);
+}
+
 TEST_CASE("Amount of cards per player at the start of the game"){
     Player p1("Alice");
     Player p2("Bob");
@@ -26,8 +35,6 @@ TEST_CASE("Check if the players got there names"){
     CHECK(p2.getname() == "Bob");
     
 }
-
-
 
 
 TEST_CASE("Amount of cards per player after 5 turns"){
@@ -62,29 +69,11 @@ TEST_CASE("Amount of cards after one turn"){
     if(p2.cardesTaken() > 0){
         CHECK(p1.cardesTaken() == 0);
     }else{
+        // No one has any cards in cardesTaken
         //cheking if we finish the game after one turn
         CHECK(((p1.stacksize() == 0) || (p2.stacksize() == 0)));
     } 
 
-}
-
-TEST_CASE("Checking whether the players are still playing after the game is over"){
-    Player p1("Alice");
-    Player p2("Bob");
-    //create new game
-    Game game(p1, p2);
-    game.playAll();
-    CHECK_THROWS(game.playTurn());
-
-}
-
-TEST_CASE("Checking the amount of cards for each player before creating a game"){
-    Player p1("Alice");
-    Player p2("Bob");
-    CHECK(p1.stacksize() == 0);
-    CHECK(p2.stacksize() == 0);
-    CHECK(p1.cardesTaken() == 0);
-    CHECK(p2.cardesTaken() == 0);
 }
 
 
@@ -114,8 +103,12 @@ TEST_CASE("Checks if someone won"){
     bool win;
     if(p1.stacksize() == 0 && p2.stacksize() == 0 && p1.cardesTaken() != p2.cardesTaken()) {
         win = true;
+        //Throwing an error if the game is still in progress
+        CHECK_THROWS(game.playTurn()); 
     }else{
         win = false;
+        //If no one won the function that prints a winner will throw an error
+        CHECK_THROWS(game.printWiner());
     }
     CHECK(win);
 
