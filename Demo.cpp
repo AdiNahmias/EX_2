cb6637ab69ff4abe2c64ede26b2af135a34a98e9
AdiNahmias <118722490+AdiNahmias@users.noreply.github.com>
2023-04-07 10:58:34 +0300

    Add files via upload

--- Demo.cpp
+++ Demo.cpp
@@ -4,12 +4,12 @@
  * @author Evgeny Hershkovitch Neiterman
  * @since 2023-03
  */
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
 using namespace std;
-
 
 #include "sources/player.hpp"
 #include "sources/game.hpp"
@@ -17,14 +17,12 @@ using namespace std;
 using namespace ariel;
 
 int main() {
-
   // Create two players with their names 
-  Player p1("Alice");
-  Player p2("Bob");
+   Player p1("Alice");
+   Player p2("Bob");
 
-  Game game(p1,p2); 
-  
-  for (int i=0;i<5;i++) {
+   Game game(p1,p2); 
+   for (int i=0;i<5;i++) {
     game.playTurn();
    }
    game.printLastTurn(); // print the last turn stats. For example:
@@ -36,4 +34,4 @@ int main() {
    game.printWiner(); // prints the name of the winning player
    game.printLog(); // prints all the turns played one line per turn (same format as game.printLastTurn())
    game.printStats();// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
- }
+}
